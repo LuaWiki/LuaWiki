dc692dfbd742140e3ae5859dda91501f35edc910
Alexander Misel <7542439+AlexanderMisel@users.noreply.github.com>
2021-11-01 22:27:01 +0800

    an upvalue function will help optimize re.gsub
    as `re.gsub` caches replace patterns by pattern+replacement

--- luawiki/preprocessor.lua
+++ luawiki/preprocessor.lua
@@ -72,15 +72,18 @@ preproc.new = function(wiki_state, template_cache)
     _tpl = {},
     _tpl_expand = {}
   }
+  
+  z.get_var = function(s)
+    return z.eval_env._var[s] or ''
+  end
 
   function z:text_visitor(node)
     if not node then return '' end
     local new_node = {}
     for i, v in ipairs(node) do
       if type(v) == 'string' then
-        new_node[i] = re.gsub(v, var_pat, function(s)
-          return self.eval_env._var[s] or ''
-        end):gsub('&(%d+);', function(tpl_num)
+        new_node[i] = re.gsub(v, var_pat, self.get_var)
+                        :gsub('&(%d+);', function(tpl_num)
           local another_preproc = preproc.new(wiki_state, self.tpl_cache)
           return another_preproc:process(self.eval_env._tpl[tonumber(tpl_num)])
         end)
